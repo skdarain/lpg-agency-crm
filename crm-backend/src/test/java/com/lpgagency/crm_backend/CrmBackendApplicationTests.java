25d5f4e1cdc2e898b775bbdc4e5513a6e67edd45
skdarain <darainnation1@gmail.com>
2024-11-12 14:34:45 +0530

    initial project setup with spring boot configuration

---
+++ crm-backend/src/test/java/com/lpgagency/crm_backend/CrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpgagency.crm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
